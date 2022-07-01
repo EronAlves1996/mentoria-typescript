d81ec65745315c66b605d04df404166526a0684f
Eron Alves <eron.alves@rocketmail.com>
2022-07-01 00:57:41 -0300

    Solved desafio2

--- src/desafios/desafio2.ts
+++ src/desafios/desafio2.ts
@@ -1,23 +1,29 @@
 // Como podemos melhorar o esse código usando TS? 
 
-let pessoa1 = {};
+interface Person {
+  nome: string;
+  idade: number;
+  profissao: string;
+}
+
+let pessoa1 = {} as Person;
 pessoa1.nome = "maria";
 pessoa1.idade = 29;
 pessoa1.profissao = "atriz"
 
-let pessoa2 = {}
+let pessoa2 = {} as Person;
 pessoa2.nome = "roberto";
 pessoa2.idade = 19;
 pessoa2.profissao = "Padeiro";
 
-let pessoa3 = {
+let pessoa3: Person = {
     nome: "laura",
-    idade: "32",
+    idade: 32,
     profissao: "Atriz"
 };
 
-let pessoa4 = {
-    nome = "carlos",
-    idade = 19,
-    profissao = "padeiro"
+let pessoa4: Person = {
+    nome: "carlos",
+    idade: 19,
+    profissao: "padeiro"
 }
